e0322cedf856d5178f58f7df9ea777fb71878d05
juumelisa <jumelisa09@gmail.com>
2022-01-28 09:37:19 +0700

    Backend Beginner

--- src/controller/vehicles.js
+++ src/controller/vehicles.js
@@ -62,14 +62,33 @@ const addVehicle = (req,res)=>{
 const updateVehicle = (req, res)=>{
     const {id} = req.params;
     const data = [req.body.name, req.body.year, req.body.cost, req.body.isAvailable, req.body.seat, req.body.type, req.body.class, id];
+    let isThere = req.body.name;
     vehicleModel.getVehicle(id, results=>{
         if(results.length>0){
-            vehicleModel.updateVehicle(data, result=>{
-                return res.send({
-                    success: true,
-                    message: 'Success add vehicle',
-                    result: `Rows affected: ${result.affectedRows}`
-                });
+            vehicleModel.checkVehicle(isThere, result=>{
+                let yesThere = 0;
+                if(result.length>0){
+                    result.forEach(element=>{
+                        if(element.year===req.body.year){
+                            yesThere++;
+                        }
+                        console.log(element);
+                    });
+                }
+                if(yesThere==0){
+                    vehicleModel.updateVehicle(data, result=>{
+                        return res.send({
+                            success: true,
+                            message: 'Success add vehicle',
+                            result: `Rows affected: ${result.affectedRows}`
+                        });
+                    });
+                }else{
+                    return res.status(400).send({
+                        success: false,
+                        message: 'Vehicle already on the list'
+                    });
+                }
             });
         }else{
             return res.status(404).send({
