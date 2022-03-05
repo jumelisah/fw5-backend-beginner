3aaf4c102cdf484d6a6f7db2ad502a15ccdbbdf5
juumelisa <jumelisa09@gmail.com>
2022-03-05 23:57:29 +0700

    edit db connection

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,13 +1,11 @@
 const mysql = require('mysql');
 const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE} = process.env;
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host : DB_HOST,
   user : DB_USER,
   password : DB_PASSWORD,
   database : DB_DATABASE
 });
-
-db.connect();
 
 module.exports = db;
