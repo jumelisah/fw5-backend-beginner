9e561774dbc1f4e134b019a6839d08a5f6254e70
juumelisa <jumelisa09@gmail.com>
2022-03-06 13:36:30 +0700

    edit auth user

--- src/controller/profile.js
+++ src/controller/profile.js
@@ -1,31 +1,21 @@
 const prof = require('../models/profile');
 
 const getProfile = (req, res)=>{
-  let {id} = req.params;
-  if(id==undefined || id==null || id==''){
-    id = req.user.id;
-  }
-  if(id>0){
-    prof.getProfile(id, result=>{
-      if(result.length>0){
-        return res.json({
-          success: true,
-          message: `User profile with ID: ${id}`,
-          result: result
-        });
-      }else{
-        return res.status(404).send({
-          success: false,
-          message: `User with ID: ${id} not found`
-        });
-      }
-    });
-  }else{
-    return res.status(400).send({
-      success: false,
-      message: 'ID should be a number greater than 0'
-    });
-  }
+  const id = req.user.id;
+  prof.getProfile(id, result=>{
+    if(result.length>0){
+      return res.json({
+        success: true,
+        message: `User profile with ID: ${id}`,
+        result: result
+      });
+    }else{
+      return res.status(404).send({
+        success: false,
+        message: `User with ID: ${id} not found`
+      });
+    }
+  });
   
 };
 
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -3,7 +3,6 @@ const profile = require('express').Router();
 const {getProfile} = require('../controller/profile');
 const { verifyUser } = require('../helpers/auth');
 
-profile.get('/', getProfile);
-profile.get('/:id', getProfile);
+profile.get('/', verifyUser, getProfile);
 
 module.exports = profile;
