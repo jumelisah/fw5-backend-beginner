923b6a10a50bc02057a7c573916fc7c7752112b4
juumelisa <jumelisa09@gmail.com>
2022-03-07 12:09:44 +0700

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require ('express');
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
 
 app.use(express.urlencoded({extended : true}));
+
+app.use(cors());
 
 app.use(require('./src/routes'));
 app.use('/uploads', express.static('uploads'));
