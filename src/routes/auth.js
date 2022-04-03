0b3caad402c47c393f3ab01986013d77253b189f
juumelisa <jumelisa09@gmail.com>
2022-04-03 11:17:51 +0700

    feat: change password

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,10 @@
 const auth = require('express').Router();
 
-const {login, forgotPassword, confirmAccount} = require('../controller/auth');
+const {login, forgotPassword, confirmAccount, changePassword} = require('../controller/auth');
 
 auth.post('/login', login);
 auth.post('/forgot-password', forgotPassword);
+auth.post('/change-password', changePassword);
 auth.post('/account-confirmation', confirmAccount);
 
 module.exports = auth;
