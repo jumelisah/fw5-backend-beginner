3b73169483366a1a98e19bc1bc55f11b162b57b9
juumelisa <jumelisa09@gmail.com>
2022-04-08 21:29:24 +0700

    feat: edit profile

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,8 +1,10 @@
 const profile = require('express').Router();
 
-const {getProfile} = require('../controller/profile');
+const {getProfile, editProfile} = require('../controller/profile');
 const { verifyUser } = require('../helpers/auth');
+const uploadImage = require('../helpers/upload');
 
 profile.get('/', verifyUser, getProfile);
+profile.patch('/', verifyUser, uploadImage('image', 1), editProfile);
 
 module.exports = profile;
