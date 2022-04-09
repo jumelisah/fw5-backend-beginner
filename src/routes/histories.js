8da9e7f286463084f48ca6b07be41e748d0a8478
juumelisa <jumelisa09@gmail.com>
2022-04-10 06:38:14 +0700

    feat: delete history

--- src/routes/histories.js
+++ src/routes/histories.js
@@ -1,6 +1,6 @@
 const histories = require('express').Router();
 
-const { getHistories, getHistory, addHistory, updateHistory, deleteHistory, getUserHistories } = require('../controller/histasync');
+const { getHistories, getHistory, addHistory, updateHistory, getUserHistories, deleteHistoryUser } = require('../controller/histasync');
 const { verifyUser } = require('../helpers/auth');
 
 histories.get('/', verifyUser, getHistories);
@@ -9,7 +9,6 @@ histories.get('/:id', verifyUser, getHistory);
 histories.post('/', verifyUser, addHistory);
 histories.patch('/:id', verifyUser, updateHistory);
 histories.patch('/', verifyUser, updateHistory);
-histories.delete('/:id', verifyUser, deleteHistory);
-histories.delete('/', verifyUser, deleteHistory);
+histories.patch('/delete/user/:id', verifyUser, deleteHistoryUser);
 
 module.exports = histories;
