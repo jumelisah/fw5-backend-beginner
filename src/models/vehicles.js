c84daa46dc5e982199e65727e332c61527850fcd
juumelisa <jumelisa09@gmail.com>
2022-04-11 16:29:11 +0700

    feat: available vehicle

--- src/models/vehicles.js
+++ src/models/vehicles.js
@@ -1,21 +1,21 @@
 const db = require('../helpers/db');
 
 exports.getVehicles = (data)=>new Promise((resolve, reject)=>{
-  db.query(`SELECT * FROM vehicles WHERE name LIKE '%${data.name}%' AND location LIKE '%${data.location}%' AND cost>=${data.cost_min} AND cost<=${data.cost_max} LIMIT ${data.limit} OFFSET ${data.offset}`, (err, res)=>{
+  db.query(`SELECT * FROM vehicles WHERE name LIKE '%${data.name}%' AND location LIKE '%${data.location}%' AND cost>=${data.cost_min} AND cost<=${data.cost_max} AND is_deleted=0 LIMIT ${data.limit} OFFSET ${data.offset}`, (err, res)=>{
     if(err) reject(err);
     resolve(res);
   });
 });
 
 exports.countData = (data)=>new Promise((resolve, reject)=>{
-  db.query(`SELECT COUNT(*) AS total FROM vehicles WHERE name LIKE '%${data.name}%' AND location LIKE '%${data.location}%' AND cost>=${data.cost_min} AND cost<=${data.cost_max}`, (err, res)=>{
+  db.query(`SELECT COUNT(*) AS total FROM vehicles WHERE name LIKE '%${data.name}%' AND location LIKE '%${data.location}%' AND cost>=${data.cost_min} AND  AND is_deleted=0 cost<=${data.cost_max}`, (err, res)=>{
     if(err) reject(err);
     resolve(res);
   });
 });
 
 exports.getVehicle = (id)=>new Promise((resolve, reject)=>{
-  db.query(`SELECT * FROM vehicles WHERE id=${id}`, (err, res)=>{
+  db.query(`SELECT * FROM vehicles WHERE id=${id} AND is_deleted=0`, (err, res)=>{
     if(err) reject(err);
     resolve(res);
   });
@@ -29,7 +29,7 @@ exports.addVehicle = (data)=>new Promise((resolve, reject)=>{
 });
 
 exports.getVehicleName = (data)=>new Promise((resolve, reject)=>{
-  db.query(`SELECT * FROM vehicles WHERE name='${data.name}' && year='${data.year}' && cost=${data.cost} && location='${data.location}'`, (err, res)=>{
+  db.query(`SELECT * FROM vehicles WHERE name='${data.name}' && year='${data.year}' && cost=${data.cost} && location='${data.location}' && is_deleted=0`, (err, res)=>{
     if(err) reject(err);
     resolve(res);
   });
@@ -50,7 +50,7 @@ exports.updateVehicle = (data, id)=>new Promise((resolve, reject)=>{
 });
 
 exports.deleteVehicle = (id)=>new Promise((resolve, reject)=>{
-  db.query('DELETE FROM vehicles WHERE id=?', [id], (err, res)=>{
+  db.query('UPDATE vehicles SET is_deleted=1 WHERE id=?', [id], (err, res)=>{
     if(err) reject(err);
     resolve(res);
   });
--- src/routes/vehicles.js
+++ src/routes/vehicles.js
@@ -10,7 +10,6 @@ vehicles.get('/:id', getVehicle);
 vehicles.post('/', verifyUser, uploadImage('image', 1), addVehicle);
 vehicles.patch('/', verifyUser, uploadImage('image', 1), updateVehicle);
 vehicles.patch('/:id', verifyUser, uploadImage('image', 1), updateVehicle);
-vehicles.delete('/', verifyUser, deleteVehicle);
-vehicles.delete('/:id', verifyUser, deleteVehicle);
+vehicles.patch('/delete/:id', verifyUser, deleteVehicle);
 
 module.exports = vehicles;
